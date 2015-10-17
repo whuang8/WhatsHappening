0a3ef9419655442587294767931cec55b51ca26e
whuang8 <iwill@purdue.edu>
2015-10-17 16:54:19 -0400

    make server corrections

--- server.js
+++ server.js
@@ -2,15 +2,22 @@
 
     // set up ========================
     var express  = require('express');
+    var http = require('http');
     var path = require('path');
-    var app      = express();                               // create our app w/ express
+    var app = express();                               // create our app w/ express
 
     app.get('/', function(req, res){
-        res.sendFile(path.join(__dirname, 'index.html'));
+        res.sendFile(path.join(__dirname, 'public/index.html'));
     });
 
-    app.use('/public', express.static(__dirname + '/public'));
+    app.use('/css', express.static(__dirname + '/public/css'));
+    app.use('/fonts', express.static(__dirname + '/public/fonts'));
+    app.use('/img', express.static(__dirname + '/public/img'));
+    app.use('/js', express.static(__dirname + '/public/js'));
+    app.use('/views', express.static(__dirname + '/public/views'));
+    app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
     // listen (start app with node server.js) ======================================
-    app.listen(1337);
+    var server = http.createServer(app).listen(1337);
+    //app.listen(1337);
     console.log("App listening on port 1337");
