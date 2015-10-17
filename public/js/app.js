cf189790b3365520cad366ab7c2ae8c907158dc0
Will Enright <wenright@purdue.edu>
2015-10-17 17:38:59 -0400

    revert

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,15 @@
 var app = angular.module('LocationApp', ['ngRoute']);
 
-app.config(function($routeProvider){$routeProvider.when('/', {controller: 'HomeController', templateURL: 'views/home.html'}).when('/photos/:id',{controller:'PhotoController', templateURL: 'views/photoController'}).otherwise({redirectTo: '/'});
-  
+app.config(function($routeProvider) {
+  $routeProvider.when('/', {
+    controller: 'HomeController', templateURL: 'views/home.html'
+  })
+
+  .when('/photos/:id', {
+    controller:'PhotoController', templateURL: 'views/photoController'
+  })
+
+  .otherwise({
+    redirectTo: '/'
+  });
 });
--- server.js
+++ server.js
@@ -1,23 +1,31 @@
 // server.js
 
-    // set up ========================
-    var express  = require('express');
-    var http = require('http');
-    var path = require('path');
-    var app = express();                               // create our app w/ express
+// set up ========================
+var express  = require('express');
+var app = express();
+var http = require('http').Server(app);
+var path = require('path');
+var io = require('socket.io')(http);
 
-    app.get('/', function(req, res){
-        res.sendFile(path.join(__dirname, 'public/index.html'));
-    });
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
-    app.use('/css', express.static(__dirname + '/public/css'));
-    app.use('/fonts', express.static(__dirname + '/public/fonts'));
-    app.use('/img', express.static(__dirname + '/public/img'));
-    app.use('/js', express.static(__dirname + '/public/js'));
-    app.use('/views', express.static(__dirname + '/public/views'));
-    app.use('/node_modules', express.static(__dirname + '/node_modules'));
+app.use('/css', express.static(__dirname + '/public/css'));
+app.use('/fonts', express.static(__dirname + '/public/fonts'));
+app.use('/img', express.static(__dirname + '/public/img'));
+app.use('/js', express.static(__dirname + '/public/js'));
+app.use('/views', express.static(__dirname + '/public/views'));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
-    // listen (start app with node server.js) ======================================
-    var server = http.createServer(app).listen(1337);
-    //app.listen(1337);
-    console.log("App listening on port 1337");
+io.on('connection', function(socket) {
+  socket.send(JSON.stringify({data: 'Welcome to the server!'}));
+
+  socket.on('message',function(data){
+    console.log(JSON.parse(data));
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on port 3000');
+});
