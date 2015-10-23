8e5d573adb0aeb46d8e0a3a744d3c759425fa012
Will Enright <wenright@purdue.edu>
2015-10-22 21:08:12 -0400

    Commented out twitter stuff

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ var express  = require('express');
 var app = express();
 var http = require('http').Server(app);
 var path = require('path');
-var io = require('socket.io')(http);
-var Twitter = require('twitter');
-var keys = require('./keys');
+// var io = require('socket.io')(http);
+// var Twitter = require('twitter');
+// var keys = require('./keys');
 
 console.log('Server starting...');
 
@@ -24,33 +24,33 @@ app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
 
 // Communicate with the client
-io.on('connection', function(socket) {
-  socket.on('message',function(message) {
-    var data = JSON.parse(message);
-    var coordinates = data.lat + ',' + data.lng + ',1mi';
-    console.log(data);
-
-    var client = new Twitter({
-      consumer_key: keys.TWITTER_CONSUMER_KEY,
-      consumer_secret: keys.TWITTER_CONSUMER_SECRET,
-      access_token_key: keys.TWITTER_ACCESS_TOKEN_KEY,
-      access_token_secret: keys.TWITTER_ACCESS_TOKEN_SECRET
-    });
-
-    client.get('search/tweets', {
-      // I wish there was a better way to do this...
-      q: 'a OR b OR c OR d OR e OR f OR g OR h OR i OR j OR k OR l OR m OR n OR o OR p OR q OR r OR s OR t OR u OR v OR w OR x OR y OR z',
-      geocode: coordinates,
-      count: 10,
-      lang: 'en'
-    }, function(error, tweets, response){
-      tweets = tweets.statuses;
-      for (var key in tweets) {
-        socket.send(JSON.stringify(tweets[key].text));
-      }
-    });
-  });
-});
+// io.on('connection', function(socket) {
+//   socket.on('message',function(message) {
+//     var data = JSON.parse(message);
+//     var coordinates = data.lat + ',' + data.lng + ',1mi';
+//     console.log(data);
+//
+//     var client = new Twitter({
+//       consumer_key: keys.TWITTER_CONSUMER_KEY,
+//       consumer_secret: keys.TWITTER_CONSUMER_SECRET,
+//       access_token_key: keys.TWITTER_ACCESS_TOKEN_KEY,
+//       access_token_secret: keys.TWITTER_ACCESS_TOKEN_SECRET
+//     });
+//
+//     client.get('search/tweets', {
+//       // I wish there was a better way to do this...
+//       q: 'a OR b OR c OR d OR e OR f OR g OR h OR i OR j OR k OR l OR m OR n OR o OR p OR q OR r OR s OR t OR u OR v OR w OR x OR y OR z',
+//       geocode: coordinates,
+//       count: 10,
+//       lang: 'en'
+//     }, function(error, tweets, response){
+//       tweets = tweets.statuses;
+//       for (var key in tweets) {
+//         socket.send(JSON.stringify(tweets[key].text));
+//       }
+//     });
+//   });
+// });
 
 http.listen(80, function(){
   console.log('listening on port 80');
